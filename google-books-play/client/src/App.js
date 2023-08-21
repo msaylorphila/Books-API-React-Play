f1343cdd8e0602891eab7ac2552bc32648b68610
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-08-21 17:56:20 -0400

    code written for api call

---
+++ google-books-play/client/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+// import { googleBooks } from './utils/Api';
+import React, { useState } from 'react';
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+   const googleBooks = async (query) => {
+    console.log('hello')
+    try {
+      const getBook = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    
+    const data = await getBook.json();
+    console.log(data);
+    }
+  
+  catch (error) {
+    console.log(error);
+  }}
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery('')
+    googleBooks(searchQuery);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <form onSubmit={handleSearch}>
+          <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="...Search"></input>
+          <button type="submit">Search</button>
+        </form>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ google-books-play/client/src/utils/Api.js
@@ -0,0 +1,13 @@
+// export const googleBooks = async () => {
+//     console.log('hello')
+//     try {
+//       const getBook = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    
+//     const data = await getBook.json();
+//     console.log(data);
+//     }
+  
+//   catch (error) {
+//     console.log(error);
+//   }}
+  
